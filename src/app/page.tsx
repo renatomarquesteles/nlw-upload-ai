9c7fa13841e649f0e1ce63908e6fd9dc55ab3128
Renato <renatomarquesteles@gmail.com>
2023-09-12 19:07:21 -0300

    chore: add load video form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import { Github } from 'lucide-react'
+import { FileVideo, Github, Upload } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
+import { Label } from '@/components/ui/label'
 
 export default function Home() {
   return (
@@ -45,7 +46,41 @@ export default function Home() {
             video
           </p>
         </div>
-        <div className="w-80"></div>
+
+        <div className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label
+              htmlFor="video"
+              className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground transition-colors hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4" />
+              Select a video
+            </label>
+
+            <input
+              type="file"
+              id="video"
+              accept="video/mp4"
+              className="sr-only"
+            />
+
+            <Separator />
+
+            <div className="space-y-1">
+              <Label htmlFor="transcription_prompt">Transcription Prompt</Label>
+              <Textarea
+                id="transcription_prompt"
+                className="min-h-20 leading-relaxed"
+                placeholder="Add keywords mentioned in the video separated by commas"
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Load video
+              <Upload className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </div>
       </main>
     </div>
   )
