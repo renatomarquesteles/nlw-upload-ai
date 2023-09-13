4c230981a34cf9e5c48a752c812c43e17e35aa1a
Renato <renatomarquesteles@gmail.com>
2023-09-12 22:59:27 -0300

    api:feat: add get all prompts request

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,10 @@
 import { fastify } from 'fastify'
 
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello'
-})
+app.register(getAllPromptsRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server listening on port 3333')
