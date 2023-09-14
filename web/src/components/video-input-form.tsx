104ecb5f02f9a920c7830cd411e810c204b437dc
Renato <renatomarquesteles@gmail.com>
2023-09-14 20:01:05 -0300

    web:feat: selects video and show thumbnail

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { ChangeEvent, useMemo, useState } from 'react'
 import { FileVideo, Upload } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
@@ -6,17 +9,49 @@ import { Separator } from '@/components/ui/separator'
 import { Textarea } from '@/components/ui/textarea'
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget
+
+    if (!files) return
+
+    setVideoFile(files[0])
+  }
+
+  const videoPreviewUrl = useMemo(() => {
+    if (!videoFile) return null
+
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
   return (
     <form className="space-y-4">
       <label
         htmlFor="video"
-        className="border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground transition-colors hover:bg-primary/5"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground transition-colors hover:bg-primary/5"
       >
-        <FileVideo className="w-4 h-4" />
-        Select a video
+        {videoPreviewUrl ? (
+          <video
+            src={videoPreviewUrl}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Select a video
+          </>
+        )}
       </label>
 
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
 
       <Separator />
 
